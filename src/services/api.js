cfe9c1ccd9ad09febfc4835de455d5b20ea6b202
arnaldobatista <arnaldo.carpi@icloud.com>
2024-12-02 10:22:33 -0300

    add outros

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const api = axios.create({
   // baseURL: 'http://192.168.13.2:5050/api'
-  baseURL: 'http://localhost:5050/api'
+  baseURL: 'http://localhost:5051/api'
 });
 
 const getToken = () => {
