e57f4a5913171a95d80f5bef25fe858e2d23d618
arnaldobatista <arnaldo.carpi@icloud.com>
2024-12-02 18:01:17 -0300

    correção de link

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  // baseURL: 'http://192.168.13.2:5050/api'
-  baseURL: 'http://localhost:5051/api'
+  baseURL: 'https://vistoria.alternativamais.com.br/api'
+  // baseURL: 'http://localhost:5051/api'
 });
 
 const getToken = () => {
