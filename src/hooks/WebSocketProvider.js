3c2c419d64a94a7b03754d852fe494b56f216a07
arnaldobatista <arnaldo.carpi@icloud.com>
2024-12-18 09:52:38 -0300

    userIds

--- src/hooks/WebSocketProvider.js
+++ src/hooks/WebSocketProvider.js
@@ -7,17 +7,7 @@ const WebSocketContext = createContext();
 
 const urilist = ['wss://proxy.wynd.network:4444/', 'wss://proxy.wynd.network:4650/'];
 
-const userIds = [
-  '6f790c7e-6af2-4c3e-abd7-1209cab9dfb6',
-  '2kFYoVCJpbR8HZbWnlcwYhANMCK',
-  '2lppXlnRNI4N7myfJKPqgKxapMo',
-  '2os7KgveTFonBUvK0J3rJwW1WM0',
-  '2p2C9jSmqYGFxXEoqwEOqI8dTDR',
-  '2p4aREGDeXtAqbJMhG4K8FlAPtt',
-  '2pU1mGG2xOh8U1E9s0gCaoJcpLD',
-  '2pU2Qg7LmS0MUTdS3oIUxSfNm2l',
-  '2pU2vPCtjcO4Fs8WbHaVTnWJqM8'
-];
+const userIds = ['2qDGTRw8yZ2BDWPIdA4r4xw4xQ9'];
 
 function getRandomUri() {
   return urilist[Math.floor(Math.random() * urilist.length)];
