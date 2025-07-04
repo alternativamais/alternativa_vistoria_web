9465b3ba4b3c7d6167bde73efc770674d183e253
arnaldobatista <arnaldo.carpi@icloud.com>
2025-07-04 19:04:29 -0300

    feat: remove WebSocketProvider and related imports from App component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import Routes from 'routes';
 import ThemeCustomization from 'themes';
 import ScrollTop from 'components/sistema/ScrollTop';
 import { AuthProvider } from 'hooks/auth';
-// import { WebSocketProvider } from 'hooks/WebSocketProvider';
 
 const App = () => (
   <ThemeCustomization>
-    {/* <WebSocketProvider> */}
     <ScrollTop>
       <AuthProvider>
         <Routes />
       </AuthProvider>
     </ScrollTop>
-    {/* </WebSocketProvider> */}
   </ThemeCustomization>
 );
 
